5a2f4d586b76bf02e88dda2f50133e1ed4a7cc17
Peter12347 <peterjelow@proton.me>
2025-04-13 13:45:29 +0200

    Typo

--- bashrc.sh
+++ bashrc.sh
@@ -2,6 +2,6 @@
 set -e
 # Back up current .bashrc file
 cp $HOME/.bashrc resources/backup/.bashrc 
-echo Your current .bashrc file has been backed up do resources/backup/.bashrc
+echo Your current .bashrc file has been backed up to resources/backup/.bashrc
 # Apply new .bashrc file
 cp resources/system/.bashrc $HOME/.bashrc
